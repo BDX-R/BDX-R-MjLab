def202a0ba22b67368a479ff0e40af84cb42b24f
Louis Le Lay <louislelay@pal-robotics.com>
2025-09-29 12:57:31 +0200

    creates project structure

---
+++ README.md
@@ -0,0 +1,8 @@
+### BDX-R in mjlab 
+
+## Quickstart
+
+Get the robot description: curl ...
+
+Lists env available
+
--- src/bdx_r_mjlab/__init__.py
+++ src/bdx_r_mjlab/__init__.py
@@ -1,2 +1,3 @@
-def main() -> None:
-    print("Hello from bdx-r-mjlab!")
+from pathlib import Path
+
+BDX_R_MJLAB_SRC_PATH: Path = Path(__file__).parent
